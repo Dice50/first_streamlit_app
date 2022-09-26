f9ebcafbfea818fc48971bd534188b0ee3cfe946
Dice50 <114185385+Dice50@users.noreply.github.com>
2022-09-26 13:21:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,8 +42,6 @@ except URLError as e:
   streamlit.error()
     
     
-#don't run anything past here while we troubleshoot
-streamlit.stop()
 
 streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
@@ -55,6 +53,10 @@ if sreamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
+#don't run anything past here while we troubleshoot
+streamlit.stop()
+    
+    
 add_my_fruit =streamlit.text_input('What fruit would you like information about?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
